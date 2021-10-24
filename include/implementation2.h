ad572fc8296f34fd67d994a021c1011a00ffed65
morlierd <damien.morlier@phelma-grenoble-inp.fr>
2021-10-24 22:59:57 +0200

    Construction fonctionnelle : Reste a lire le dictionnaire et remplir l'arbre + un print correct de l'arbre

--- include/implementation2.h
+++ include/implementation2.h
@@ -4,6 +4,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 #define ALPHABET 26
 
 
@@ -13,5 +14,13 @@ typedef struct noeud{
 }noeud_t,*arbreprefixe_t;
 
 
-
+noeud_t* creer_noeud(char c);
+arbreprefixe_t creer_arbre();
+void destruct_noeud(noeud_t* p);
+int arbre_est_vide(arbreprefixe_t a);
+arbreprefixe_t creer_arbre();
+void parcours_infixe(arbreprefixe_t a,int espace,int n);
+int indice_caractere(char c);
+void inserer_lettre(arbreprefixe_t* a, char c);
+void inserer_phrase(arbreprefixe_t* a, char* mot);
 #endif
--- src/implementation2.c
+++ src/implementation2.c
@@ -1,24 +1,74 @@
-#include "implementation2."
+#include "implementation2.h"
+
+#include <string.h>
 
 
-
-
-noeud* creer_noeud(char c){
-  noeud*p=calloc(1,sizeof(*p));
-  char s =strdup(strtolower(c));
+noeud_t* creer_noeud(char c){
+  noeud_t*p=calloc(1,sizeof(*p));
+  char s = tolower(c);
   p->lettre=s;
   return p;
 }
 
 
-void* destruct_noeud(noeud* p){
-  free(p->lettre);
+void destruct_noeud(noeud_t* p){
   free(p);
 }
 
-
-arbresuffixe_t creer_arbre(){
+int arbre_est_vide(arbreprefixe_t a){
+  return a==NULL;
+}
+arbreprefixe_t creer_arbre(){
   arbreprefixe_t p=creer_noeud(' ');
   return p;
+}
+void parcours_infixe(arbreprefixe_t a,int espace,int n){
+  if(!arbre_est_vide(a)){
+    for(int i=0;i<13;i++){
+      espace+=n;
+      parcours_infixe(a->alp[i],espace,n);
+      printf("\n");
+    }
+    for(int i=n;i<espace;i++){
+      printf(" ");
+    }
+    printf("%c\n", a->lettre);
+    for(int i=13;i<26;i++){
+    parcours_infixe(a->alp[i],espace,n);
+    }
+  }
+}
+int indice_caractere(char c){
+  char s =tolower(c);
+  return (int)(s-'a');
+}
+void inserer_lettre(arbreprefixe_t* a, char c){
+  int indice =indice_caractere(c);
+  if((*a)->alp[indice]==NULL){
+  (*a)->alp[indice]=creer_noeud(c);
+  }
+}
+void inserer_phrase(arbreprefixe_t* a, char* mot){
+  arbreprefixe_t p=*a;
+  int indice;
+  for(int i=0;mot[i]!='\0';i++){
+    indice=indice_caractere(mot[i]);
+    inserer_lettre(&p,mot[i]);
+    p=p->alp[indice];
+  }
+}
+
+int main(){
+  arbreprefixe_t a=creer_arbre();
+  //a->alp[1]=creer_noeud('b');
+  inserer_lettre(&a,'a');
+  inserer_lettre(&a,'b');
+  inserer_lettre(&a,'d');
+  inserer_lettre(&a,'c');
+  inserer_phrase(&a,"bonjour");
+//  parcours_infixe(a,0,1);
+  printf("%d\n",'b'-'a');
+  printf("%c %c %c %c %c \n",a->alp[1]->lettre,a->alp[1]->alp[14]->lettre,a->alp[1]->alp[14]->alp[13]->lettre,a->alp[1]->alp[14]->alp[13]->alp[9]->lettre,a->alp[1]->alp[14]->alp[13]->alp[9]->alp[14]->lettre);
+  return EXIT_SUCCESS;
 }
 //Construire une table de hachage qui contient les suffixes déja obtenus(penser au free qui decremente juste le dernier un compteur)
